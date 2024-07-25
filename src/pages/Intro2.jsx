65cd9ec63f5c94a17de465a0e0bb0d8630e393be
Perrine Deserville <mondierep@gmail.com>
2024-07-25 17:21:21 +0200

    pages 1et2 ok. Manque lien sur page 2

--- src/pages/Intro2.jsx
+++ src/pages/Intro2.jsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router-dom";
+
 import "./Intro2.css"
 
 function Intro2() {
     return (
         <div id="intro2-body">
-            <h1>Bienvenue sur <span className="text-logo">Pigeon Prime</span></h1>
+            <h2>Une nouvelle aire est née,<br/>
+            nous vous offrons la possibilité de délivrer vos messages et/ou colis<br/>
+            de façon personnalisée en fonction de vos envies et de vos moyens</h2>
+            <h2 className="intro2-bienvenue">Bienvenue sur :</h2>
+            <Link to="/">
+            <img src="src/assets/pigeon-prime.png" alt ="logo pigeon prime" />
+            </Link>
         </div>
     )
 }
